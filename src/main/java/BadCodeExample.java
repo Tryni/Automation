c6c4b8d183d8c19476dd9183821613f67c5bdc99
Olena Trunova <olenatr@payoneer.com>
2019-03-17 15:45:05 +0200

    Automation lesson1

---
+++ src/main/java/BadCodeExample.java
@@ -0,0 +1,17 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebElement;
+
+public class BadCodeExample {
+    public static void main(String[] args) {
+        System.out.println("Hello World!!!");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.google.com");
+        WebElement element = driver.findElement(By.name("q"));
+        element.sendKeys("Selenium");
+        element.submit();
+
+    }
+
+}
